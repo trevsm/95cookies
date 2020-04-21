d228aa14944f1773d3006e85dedccd6105d8eddc
dev <16tsmith55@gmail.com>
2020-04-20 19:32:44 -0600

    added background to welcome

--- src/component/footer/Popup.js
+++ src/component/footer/Popup.js
@@ -7,29 +7,45 @@ function Popup() {
       <div className="tab"></div>
       <div className="bottom"></div>
       <div className="social">
-        <a href="https://google.com/" className="button is-medium is-github">
+        <a
+          target="new"
+          href="https://github.com/"
+          className="button is-medium is-github"
+        >
           <span className="icon">
             <i className="fab fa-github"></i>
           </span>
           <span>GitHub</span>
         </a>
-        <a href="https://google.com/" className="button is-medium is-facebook">
+        <a
+          target="new"
+          href="https://facebook.com/"
+          className="button is-medium is-facebook"
+        >
           <span className="icon">
             <i className="fab fa-facebook"></i>
           </span>
-          <span>GitHub</span>
+          <span>Facebook</span>
         </a>
-        <a href="https://google.com/" className="button is-medium is-vimeo">
+        <a
+          target="new"
+          href="https://vimeo.com/"
+          className="button is-medium is-vimeo"
+        >
           <span className="icon">
             <i className="fab fa-vimeo"></i>
           </span>
-          <span>GitHub</span>
+          <span>Vimeo</span>
         </a>
-        <a href="https://google.com/" className="button is-medium is-youtube">
+        <a
+          target="new"
+          href="https://youtube.com/"
+          className="button is-medium is-youtube"
+        >
           <span className="icon">
             <i className="fab fa-youtube"></i>
           </span>
-          <span>GitHub</span>
+          <span>Youtube</span>
         </a>
       </div>
       <div className="copy">&copy; 2020, 95Cookies</div>
